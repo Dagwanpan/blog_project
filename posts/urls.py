6abdd9b9c9b3f5d81167b4f29e779a48e413d196
Dagwanpan <dagwanpanandanladi@gmail.com>
2026-04-28 13:11:05 +0100

    Added template, statics and accounts app

--- posts/urls.py
+++ posts/urls.py
@@ -7,5 +7,5 @@ urlpatterns = [
     path('create/', create_post, name='create_post'),
     path('update/<int:id>/', update_post, name='update_post'),
     path('post/<int:id>/', post_detail, name='post_detail'),
-    path('delete/<int:id>/', delete_post, name='delete_post')
+    path('delete/<int:id>/', delete_post, name='delete_post'),
 ]
