49f04ad7715ab2470603b0cfe4292ae4f163de34
Dagwanpan <dagwanpanandanladi@gmail.com>
2026-04-28 13:54:37 +0100

    Added create views, urls for accounts registrations

--- accounts/views.py
+++ accounts/views.py
@@ -1,3 +1,24 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth import login, logout
+from .forms import RegisterForm
 
-# Create your views here.
+# Registration form
+def register(request):
+    if request.method == 'POST':
+        form = RegisterForm(request.POST)
+        
+        if form.is_valid():
+            user = form.save(commit=False)
+            login(request, user)
+            return redirect('home')
+    else:
+        form = RegisterForm()
+    
+    return render(request, 'accounts/register.html', {
+        'form': form
+    })
+
+# Logout User
+def logout_user(request):
+    logout(request)
+    return redirect('home')
--- blog_project/settings.py
+++ blog_project/settings.py
@@ -122,6 +122,6 @@ LOGIN_URL = 'login'
 
 LOGIN_REDIRECT_URL = 'home'
 
-MEDIA_URL = '/media'
+MEDIA_URL = '/media/'
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
