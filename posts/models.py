001924d955240006cff26947f14a686d4657f0bd
Dagwanpan <dagwanpanandanladi@gmail.com>
2026-05-01 23:43:47 +0100

    Create Post model and register it in Django admin

--- posts/models.py
+++ posts/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
-# Create your models here.
+# Post model.
+class Post(models.Model):
+    title = models.CharFied(max_length=200)
+    content = models.TextField()
+    created_at = models.DateTimeField(auto_add_now=True)
+    
+    def __str__(self):
+        return self.title
+    
