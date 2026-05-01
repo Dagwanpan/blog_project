ebb4e37d4a639fa24911a75c3bcfe62b727edb43
Dagwanpan <dagwanpanandanladi@gmail.com>
2026-05-01 23:50:08 +0100

    Implemented home page view to display all posts from the database

--- posts/views.py
+++ posts/views.py
@@ -1,3 +1,10 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, get_object_or_404
+from .model import Post
+from .form import PostForm
 
-# Create your views here.
+# Home page
+def home(request):
+    posts = Post.objects.all()
+    return render(request, 'posts/home.html', {
+        'posts': posts
+    })
