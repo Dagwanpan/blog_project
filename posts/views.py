6858ffb252848194736192140f72b4a8e9840bd6
Dagwanpan <dagwanpanandanladi@gmail.com>
2026-05-01 23:53:58 +0100

    Implement view to handle post creation

--- posts/views.py
+++ posts/views.py
@@ -8,3 +8,18 @@ def home(request):
     return render(request, 'posts/home.html', {
         'posts': posts
     })
+    
+# Creat post
+def create_post(request):
+    if request.method == 'POST':
+        form = PostForm(request.POST)
+        
+        if form.is_valid():
+            form.save()
+            return redirect('home')
+    else:
+        form = PostForm()
+    
+    return render(request, 'posts/create_post.html', {
+        'form': form
+    })
