d6ba11697c24d5210abc06d10f4aa6e7b40dcc82
Dagwanpan <dagwanpanandanladi@gmail.com>
2026-05-02 00:37:49 +0100

    Add user registration view with automatic login after signup

--- accounts/views.py
+++ accounts/views.py
@@ -1,3 +1,19 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth import login, logout
+from .forms import RegisterForm
 
-# Create your views here.
+# Register Form.
+def register(request):
+    if request.method == 'POST':
+        form = RegisterForm(request.POST)
+        
+        if form.is_valid():
+            user = form.save()
+            login(request, user)
+            return redirect('home')
+    else:
+        form = RegisterForm()
+    
+    return render(request, 'accouts/register.html', {
+        'form': form        
+    })
