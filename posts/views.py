b32b09e23741b56c042b4603f487a6989117c2b5
Dagwanpan <dagwanpanandanladi@gmail.com>
2026-05-02 03:02:00 +0100

    Implemented full comment system in post detail view including authentication checks and nested reply support

--- posts/views.py
+++ posts/views.py
@@ -1,11 +1,16 @@
 from django.shortcuts import render, redirect, get_object_or_404
+from django.db.models import Count, Avg
 from django.contrib.auth.decorators import login_required
-from .models import Post
-from .forms import PostForm
+from .models import Post, Comment
+from .forms import PostForm, CommentForm
 
 # Home page
 def home(request):
-    posts = Post.objects.all()
+    posts = Post.objects.select_related('user').prefetch_related(
+        'comments').annotate(
+        total_comments=Count('comments')
+    ).order_by('-created_at')
+        
     return render(request, 'posts/home.html', {
         'posts': posts
     })
@@ -53,8 +58,36 @@ def update_post(request, id):
 def post_detail(request, id):
     post = get_object_or_404(Post, id=id)
     
+    if request.method == 'POST':
+        
+        if not request.user.is_authenticated:
+            return redirect('login')
+        
+        form = CommentForm(request.POST)
+        
+        if form.is_valid():
+            comment = form.save(commit=False)
+            comment.post = post
+            comment.user = request.user
+        
+        parent_id = request.POST.get('parent_id')
+        if parent_id:
+            comment.parent = Comment.objects.get(id=parent_id)
+        
+        comment.save()
+        return redirect('post_detail', id=post.id)
+    
+    else:
+        form = CommentForm()
+        
+    comments = post.comments.select_related('user').filter(
+        parent=None
+        ).order_by('-created_at')        
+    
     return render(request, 'posts/post_detail.html', {
-        'post': post
+        'post': post,
+        'comments': comments,
+        'form': form
     })
 
 # Delete post by Id
