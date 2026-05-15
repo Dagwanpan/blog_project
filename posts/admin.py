757e747f08bce1bd8e422a7b07214eea1ce22ee0
Dagwanpan <dagwanpanandanladi@gmail.com>
2026-05-15 11:41:39 +0100

    Register profile model

--- posts/admin.py
+++ posts/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
-from .models import Post, Comment
+from .models import Post, Comment, Profile
 
-# Register the app.
+# Register the apps.
 admin.site.register(Post)
 admin.site.register(Comment)
+admin.site.register(Profile)
